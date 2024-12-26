d34328c1dc598187f68861b9cca2de710c49e850
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2024-12-26 18:37:22 +0600

    Add Tetromino shader files for vertex and fragment processing

---
+++ resources/shaders/Tetromino.glsl
@@ -0,0 +1,19 @@
+# shader vertex
+# version 330 core
+
+layout(location = 0) in vec4 a_Position;
+
+uniform mat4 u_MVP;
+
+void main() {
+    gl_Position = u_MVP * a_Position;
+}
+
+# shader fragment
+# version 330 core
+
+out vec4 FragColor;
+
+void main() {
+    FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+};
